abffda8405a0065abc09568d3a1d06a0d46eafb3
Leonardo1903 <fmartins.leonardo@gmail.com>
2021-09-21 15:21:58 -0300

    resolvendo conflitos

--- src/Components/header.js
+++ src/Components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import carrinho from '../img/twotone_shopping_cart_white_24dp.png'
 import account from '../img/twotone_account_circle_white_24dp.png'
-import terra from '../img/Imagens-do-planeta-Terra-em-alta-resolução-6.jpg'
+import terra from '../img/Imagens-do-planeta-Terra-em-alta-resolução-4.jpg'
 
 
 
@@ -29,7 +29,7 @@ const BarraEsquerda = styled.div`
     margin-right: 200px; 
 `;
 const BarraCentro = styled.div`
-    width: %;
+    width: 33.333%;
     text-align: center;
 `;
 
